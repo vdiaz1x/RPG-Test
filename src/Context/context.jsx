13cdae08ef402a5d9f092ab9cabd893fb7d09a64
Vinicio Diaz <vdiaz1x@gmail.com>
2019-12-31 10:13:45 -0500

    Revert "Add context"

--- src/Context/context.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import values from './values';
-
-const Context = React.createContext(values);
-
-export const Provider = Context.Provider
-export const Consumer = Context.Consumer
-export default Context;
